6c5e14076541fe435cd97f5794ddc1a9199b239b
Simon-Pierre Alepin <s.alepin@hotmail.com>
2021-05-02 00:23:55 -0400

    fix(web-colors) Fixes percent to  toChannel conversion.

--- packages/web-colors/src/utils.ts
+++ packages/web-colors/src/utils.ts
@@ -4,7 +4,7 @@ export const toChannel = (
 ): number | undefined => {
   if (!isValidChannel(value) && !isPercent) return;
   if (isValidChannel(value)) return toFloat(value as number, decimals);
-  return toFloat((100 / 255) * toFloat((value as string).replace('%', ''), decimals), decimals);
+  return toFloat((255 / 100) * parseFloat((value as string).replace('%', '')), decimals);
 };
 
 export const toFloat = (value: number | string, decimals: number = 2): number => {
@@ -13,7 +13,7 @@ export const toFloat = (value: number | string, decimals: number = 2): number =>
 
 export const toAlphaChannel = (value: unknown, decimals: number = 2): number | undefined => {
   if (undefined === value) return;
-  if (isPercent(value)) return toChannel(value);
+  if (isPercent(value)) return toChannel(value, decimals);
   if (isValidChannel(Number(value) * 255)) return toFloat(Number(value) * 255, decimals);
   return;
 }
@@ -22,6 +22,8 @@ export const isValidChannel = (value: unknown): boolean => {
   return (`${value}`.length > 0 && !isNaN(Number(value)) && Number(value) >= 0 && Number(value) <= 255);
 }
 
-export const isPercent = (value: unknown): boolean => {
-  return typeof value === 'string' && value.indexOf('%') > -1;
+export const isPercent = (value: unknown, decimals: number = 2): boolean => {
+  if (typeof value !== 'string' || `${value}`.indexOf('%') === -1) return false;
+  const pct = toFloat((value as string).replace('%', ''), decimals);
+  return pct >= 0 && pct <= 100;
 }
